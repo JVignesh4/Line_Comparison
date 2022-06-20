39b35046a98e506e5154c79f8776651e143b11f7
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-20 09:43:17 +0530

    Added: Length Of The Lines

--- LineComparison.java
+++ LineComparison.java
@@ -4,6 +4,18 @@ import static java.lang.Math.sqrt;
 
 public class LineComparison {
     public static void main(String[] args) {
+        double lineLength = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter The value Of x1 : ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Enter The value Of x2 : ");
+        double x2 = scanner.nextDouble();
 
+        System.out.print("Enter The value Of y1 : ");
+        double y1 = scanner.nextDouble();
+        System.out.print("Enter The value Of y2 : ");
+        double y2 = scanner.nextDouble();
+        lineLength = sqrt( (x2 - x1) * 2 + (y2 - y1) * 2);
+        System.out.println(lineLength);
     }
 }
